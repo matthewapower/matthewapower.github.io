b9d2052c2cea0c30c74d5cbcd991ca69db9a3191
Matt Power <matthewallenpower@gmail.com>
2018-08-30 18:17:41 -0400

    Take content off arena per their guidelines.

--- main.js
+++ main.js
@@ -2,25 +2,6 @@ $(document).ready(function(){
   var blocks = [],
       container = $('.overlay');
 
-  $.getJSON('https://api.are.na/v2/channels/portfolio-work', function(data) {
-    blocks = data.contents.reverse();
-    for (i = 0; i < blocks.length; i++ ) {
-      var block = data.contents[i],
-          link = block.source,
-          img = block.image.large.url,
-          title = block.title,
-          description = block.description_html;
-
-      if (link == null) { link = img; } else { link = link.url; }
-
-      if (description == null) { description = ' '; }
-
-      container.append(
-        '<a class="block" href="'+ link +'" target="_blank"><img class="block--img" src="' + img +'"/></a><h2 class="block--title">' + title + '</h2><div>' + description + '</div>'
-      );
-    }
-  });
-
   $('.open--boii').click(function(){ $('.overlay').addClass('active'); });
   $('.close--boii').click(function(){ $('.overlay').removeClass('active'); });
 });
